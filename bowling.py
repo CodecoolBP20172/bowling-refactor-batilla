a67339565d6309889c35aa012148b7f509c175b5
tilla.bakos <tilla.bakos@gmail.com>
2017-08-05 11:09:04 +0200

    added parameter string conversion to upper case eliminating unnecessary case check

--- bowling.py
+++ bowling.py
@@ -1,12 +1,15 @@
 def score(game):
+    game = str.upper(game)
     result = 0
     frame = 1
+    last_count = []
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
             result += 10 - last
         else:
             result += get_value(game[i])
+            print(result)
         # if not in_first_half:
             # frame += 1
         if frame < 10  and get_value(game[i]) == 10:
@@ -19,6 +22,8 @@ def score(game):
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
+        last_count.append(last)
+        print(last_count, "last")
         if not in_first_half:
             frame += 1
         if in_first_half == True:
@@ -41,3 +46,6 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
+
+
+print(score("1/35XXX458/X3/23"))
